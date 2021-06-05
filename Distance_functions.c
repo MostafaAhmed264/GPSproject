d9f3f2f23fc2a29f3e7fb8d60f6b9c849e5c0d71
mariam <mariam>
2021-06-05 17:49:54 +0200

    Distance functions

---
+++ Distance_functions.c
@@ -0,0 +1,34 @@
+#include "tm4c123gh6pm.h"
+#include <math.h>
+#include <stdio.h>
+#define PI 3.141592653589793238
+int total_Distance = 100; 
+float getDistanceFromLatLonInM(lat1,lon1,lat2,lon2) {
+  int R = 6371; // Radius of the earth in km
+  float dLat = ((lat2-lat1)*PI)/180;  // deg2rad below
+  float dLon = ((lon2-lon1)*PI)/180; 
+  float a = 
+    sin(dLat/2) * sin(dLat/2) +
+    cos(((lat1)*PI)/180) * cos((lat2)*PI)/180) * 
+    sin(dLon/2) * sin(dLon/2)
+    ; 
+  float c = 2 * atan2(sqrt(a), sqrt(1-a)); 
+  float d = R * c; // Distance in km
+  return d * 1000;
+}
+
+void calcTotalDistance(float distance){ 
+	
+	total_Distance += distance
+}
+
+
+void lightLed(){
+    if (total_Distance > 100){ 
+        GPIO_PORTF_DATA_R |= 0x04; 
+    }
+}
+int int_total_Distance(){ 
+	
+	return (int)(total_Distance); 
+}
